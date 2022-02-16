5f723e54ad7eaeac5b02f56af056f87c577828fb
Benjarat Burapajetrak <85210845+geneyeans@users.noreply.github.com>
2022-02-16 14:10:29 +0700

    Add files via upload

---
+++ Homework12_1.java
@@ -0,0 +1,12 @@
+package homework_12;
+
+public class Homework12_1 {
+
+class StringAnt {
+	public static void main(String[] args) {
+		char[] table = {'w','o','m','e','n'};
+		String bat = new String(table);
+		System.out.println(bat); 
+	}
+}
+}
